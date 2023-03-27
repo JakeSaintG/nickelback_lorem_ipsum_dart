d663c32c0e609f5c80eb55001f03c2946d1259c6
Jake Saint Germain <jakesaint27@gmail.com>
2023-03-27 10:54:11 -0400

    initialize new proj

---
+++ bin/nickback_lorem_ipsum_dart.dart
@@ -0,0 +1,5 @@
+import 'package:nickback_lorem_ipsum_dart/nickback_lorem_ipsum_dart.dart' as nickback_lorem_ipsum_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${nickback_lorem_ipsum_dart.calculate()}!');
+}
---
+++ test/nickback_lorem_ipsum_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:nickback_lorem_ipsum_dart/nickback_lorem_ipsum_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
