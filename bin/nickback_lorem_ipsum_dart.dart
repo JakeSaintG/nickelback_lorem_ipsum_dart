4e4dcbb08d5197b57c75c517bed7e51ff04d50a7
Jake Saint Germain <jakesaint27@gmail.com>
2023-03-27 11:06:49 -0400

    stub out plan

--- bin/nickback_lorem_ipsum_dart.dart
+++ bin/nickback_lorem_ipsum_dart.dart
@@ -1,5 +1,24 @@
-import 'package:nickback_lorem_ipsum_dart/nickback_lorem_ipsum_dart.dart' as nickback_lorem_ipsum_dart;
+import 'package:nickback_lorem_ipsum_dart/nickback_lorem_ipsum_dart.dart'
+    as nickback_lorem_ipsum_dart;
 
 void main(List<String> arguments) {
   print('Hello world: ${nickback_lorem_ipsum_dart.calculate()}!');
+
+  //Read from songslist.json
+  //Loop through the list
+  //Store in List<Song>
+
+  //Initial welcome message
+  //GenerateQuip
+
+  //Main While Loop
+  //  How many lines do you want to generate?
+  //  parse input - handle error (negatives, zero, non-int)
+  //    allow for quit commands
+  //  Loop through song lines the amount of input times
+  //  Store and manipulate
+  //    Adds a space between each line added except the first one.
+  //    Some songs have an "I" as a pause that then goes into the next line.
+  //    Add a period to every second line added to the text block. Prevent it from adding a period to a weird character.
+  //    Double check against C# version
 }
